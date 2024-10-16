72ca3127329ad6cfa16fa7cbe00786acbcccce32
Babu Sankar Nithin Sankar <nithinsankar.b@outlook.com>
2024-10-16 15:18:15 +0800

    Setting up basic file structure

---
+++ src/entities/User.java
@@ -0,0 +1,28 @@
+public class User
+{
+    private String hospitalID;
+    private String password;
+
+    public User(String hospitalID, String password)
+    {
+        this.hospitalID = hospitalID;
+        this.password = password;
+    }
+
+    public String getHospitalID() {
+        return hospitalID;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setHospitalID(String hospitalID) {
+        this.hospitalID = hospitalID;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
