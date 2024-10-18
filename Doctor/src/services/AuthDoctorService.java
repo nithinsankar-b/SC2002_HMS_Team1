05a970e42e0d3d39f2130fe73f15d4ab29ad4835
Risha Sunil Shetty <164110710+RISHASUN001@users.noreply.github.com>
2024-10-18 12:47:38 +0800

    Update AuthDoctorService.java

--- Doctor/src/services/AuthDoctorService.java
+++ Doctor/src/services/AuthDoctorService.java
@@ -28,8 +28,4 @@ public class AuthDoctorService extends AuthService {
         AuthStore.setCurrentUser(doc);
         return true;
     }
-    //NO NEED TO GET THE HOSPITALID AS PARAMETER 
-    //This is because actually the object doc in this case is being retrieved from the map via the hospitalID
-    //so if it does not exist--> object = null therefore false will be returned.If it exists but password wrong then false again.
-    //so this way both hospitalID and password is verified 
 }
