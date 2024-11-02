6c56142438d6adf71ce5be982699c03d37b78535
Chang Lin <changlintan30@gmail.com>
2024-11-02 16:15:28 +0800

    Updated necessary inventory and model files after merge

--- src/models/InventoryDisplay.java
+++ src/models/InventoryDisplay.java
@@ -1,22 +1,19 @@
 package models;
 
 import enums.InventoryStatus;
-import enums.ReplenishmentStatus;
 
 public class InventoryDisplay {
     private String name;
     private int quantity;
     private InventoryStatus status;
-    private ReplenishmentStatus replenishmentStatus; // New field for ReplenishmentStatus
 
-    public InventoryDisplay(String name, int quantity, InventoryStatus status, ReplenishmentStatus replenishmentStatus) {
+    public InventoryDisplay(String name, int quantity, InventoryStatus status) {
         this.name = name;
         this.quantity = quantity;
         this.status = status;
-        this.replenishmentStatus = replenishmentStatus; // Initialize the new field
     }
 
-    // Getters
+    // Getters and Setters
     public String getName() {
         return name;
     }
@@ -27,9 +24,5 @@ public class InventoryDisplay {
 
     public InventoryStatus getStatus() {
         return status;
-    }
-
-    public ReplenishmentStatus getReplenishmentStatus() { // Getter for the new field
-        return replenishmentStatus;
     }
 }
