11d497366c6b68becd384dd6e970d15da67f6358
Risha Shetty <risha.shetty@icloud.com>
2024-11-02 18:16:24 +0800

    updated pharmacist and doctor

---
+++ src/views/PatientMedicalRecordView.java
@@ -0,0 +1,19 @@
+package views;
+
+import services.MedicalRecordService;
+
+
+public class PatientMedicalRecordView  {
+
+
+    private final MedicalRecordService medicalRecordService;
+    
+    public PatientMedicalRecordView(MedicalRecordService medicalRecordService) {
+        this.medicalRecordService = medicalRecordService;    }
+        //Viewing of MedicalRecords
+    public void printMedicalRecords(String patientID) {       
+    	System.out.println("Displaying Medical Records for Patient ID: " + patientID);
+        MedicalRecordService m=new MedicalRecordService();       
+        m.printMedicalRecord(patientID);
+    }
+ }
